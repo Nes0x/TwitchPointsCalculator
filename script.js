13ad484c4ccb956daae24377ca1dd0c8629b431a
Nes0x <66802265+Nes0x@users.noreply.github.com>
2022-07-24 14:32:22 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+const result = document.getElementById("result");
+
+window.onload = function () {
+    document.getElementById("choice").addEventListener("change", addTags);
+}
+
+function addTags(event) {
+    let element = document.getElementById("calculator-labels");
+    let value = event.target.value;
+
+    switch (value) {
+        case "time":
+            element.innerHTML = `
+                   <br><br>
+                   <label for="days">Dni</label>
+                   <input type="number" min="0" id="days">
+                   <br><br>
+                   <label for="hours">Godziny</label>
+                   <input type="number" min="0" id="hours">
+                   <br><br>
+                   <label for="minutes">Minuty</label>
+                   <input type="number" min="0" id="minutes">
+                   <br><br>
+                   <button id="calculate" name="time" type="submit">Kliknij aby wyliczyć punkty</button>
+            `
+            break;
+        case "points":
+            element.innerHTML = `
+                   <br><br>
+                   <label for="points">Ilość punktów</label>
+                   <input type="number" min="0" id="points">
+                   <br><br>
+                   <button id="calculate" name="points" type="submit">Kliknij aby wyliczyć czas</button>
+            `
+            break;
+        default:
+            element.innerHTML = "";
+            break;
+    }
+    result.innerText = "";
+    document.getElementById("calculate").addEventListener("click", calculate);
+}
+
+
+function calculate(event) {
+   let name = event.target.name;
+   switch (name) {
+       case "time":
+           let days = document.getElementById("days").value;
+           let hours = document.getElementById("hours").value;
+           let minutes = document.getElementById("minutes").value;
+           let calculatePoints = ((days * 24 * 60) + (hours * 60) + Number(minutes)) * 3;
+           result.innerText = "Ilość punktów które zdobędziesz w podany czas to: " + Math.trunc(calculatePoints);
+           break;
+       case "points":
+           let points = document.getElementById("points").value;
+           result.innerText = getTimeFromMinutes(Math.trunc((points / 60) * 20));
+           break;
+   }
+}
+
+function getTimeFromMinutes(minutes) {
+    let time = "";
+    if (Math.trunc(minutes / 1440) > 0) {
+        time += "\n" + Math.trunc(minutes / 1440) + " dni";
+        minutes -= Math.trunc((minutes / 1440)) * 1440;
+    }
+    if (Math.trunc(minutes / 60) > 0 && !(Math.trunc(minutes / 1440) > 0)) {
+        time += "\n" + Math.trunc(minutes / 60) + " godzin/y";
+        minutes -= Math.trunc((minutes / 60)) * 60;
+    }
+    time += "\n" + Math.trunc(minutes) + " minut/y";
+    return time;
+}
